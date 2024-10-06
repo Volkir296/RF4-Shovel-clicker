9057ba76e2687fd7947f48fc71d43ceda15d32e5
MarkXXIX <40279883+Volkir296@users.noreply.github.com>
2024-10-06 16:47:18 +0300

    Update скрипта
    Рассчёт таймингов

--- Main/main.py
+++ Main/main.py
@@ -1,29 +1,33 @@
 import pyautogui
 import time
 
-seconds = 10
-
-def timer(seconds):
-    print(f"Таймер запущен на {seconds} секунд.")
-    time.sleep(seconds)
-    print("Время вышло!")
-
-timer(seconds)
-
+# Переменные
 foodOne = '4' # Переменная для еды
-foodTwo = '5' #
+foodTwo = '5' # Переменная для еды 2
 shovel = '6' # Переменная для лопаты
+#seconds = 10 # Время для таймера'
+flag = False
 
+# 30 секунд на полном солнышке
+# 4 секунды от копания до пробела
+
+
+# Определение середины экрана
 scrWidth, scrHeight = pyautogui.size()
 print(scrWidth, scrHeight) # Разрешение экрана
-
 centrW, centrH = scrWidth/2, scrHeight/2
 print(centrW,centrH) # Середина экарана
 
-pyautogui.moveTo(centrW,centrH) # Перемещение курсора по XY на центр экрана
-
-pyautogui.click() # Клик мыши
-
-pyautogui.press('space')
-
-pyautogui.press(foodOne)
+while True:
+    if flag == False:
+        flag = True
+        time.sleep(5)
+    else:
+        pyautogui.moveTo(centrW,centrH) # Перемещение курсора по XY на центр экрана
+        pyautogui.click() # Клик мыши
+        time.sleep(4) #Таймер
+        pyautogui.press('space')
+        time.sleep(1) #Таймер
+        pyautogui.press(foodOne)
+        pyautogui.press(foodTwo)
+        time.sleep(30) #Таймер
